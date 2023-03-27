9c7af177386e3484c79d698a9e512a1ff0fb5ab2
d3trim3ntal <obsidianblackrose@gmail.com>
2023-03-27 11:43:35 -0700

    expert only
    expert only

--- Unit3SoundEffectZentenoP3/Assets/Scripts/PlayerController.cs
+++ Unit3SoundEffectZentenoP3/Assets/Scripts/PlayerController.cs
@@ -17,6 +17,7 @@ public class PlayerController : MonoBehaviour
     private AudioSource playerAudio;
     public bool doubleJumpUsed = false;
     public float doubleJumpForce;
+    public bool doubleSpeed = false;
 
     // Start is called before the first frame update
     void Start()
@@ -46,6 +47,17 @@ public class PlayerController : MonoBehaviour
             playerRb.AddForce(Vector3.up * doubleJumpForce, ForceMode.Impulse);
             playerAnim.Play("Running_Jump", 3,0f);
             playerAudio.PlayOneShot(jumpSound, 1.0f);
+        }
+
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            doubleSpeed = true;
+            playerAnim.SetFloat("Speed_Multiplier", 2.0f);
+        }
+        else if (doubleSpeed)
+        {
+            doubleSpeed = false;
+            playerAnim.SetFloat("Speed_Multiplier", 1.0f);
         }
     }
 
